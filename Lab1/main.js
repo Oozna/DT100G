ea09b8e9e26ec0f70154556497cd7978e40da166
Oozna <johanssonericwilliam@gmail.com>
2022-01-25 15:13:03 +0100

    Lab 1 klar

--- Lab1/main.js
+++ Lab1/main.js
@@ -3,21 +3,21 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("business_card").style.display = "none"; //Hides card by default
 
-    //Creates a card based on the user's form
+    //Skapar ett visitkort utifrån inmatade värden när skriv ut-knappen trycks
     document.getElementById("print_button").addEventListener("click", function () {
-        //Creates the card
+        //Skapar själva kortet
         document.getElementById("business_card").style.display = "block";
         document.getElementById("business_card_company").innerHTML = document.getElementById("form_company_input").value;
         document.getElementById("business_card_name").innerHTML = document.getElementById("form_first_name_input").value + " " + document.getElementById("form_last_name_input").value;
         document.getElementById("business_card_title").innerHTML = document.getElementById("form_title_input").value;
-        document.getElementById("business_card_phone").innerHTML = "Phone number: " + document.getElementById("form_phone_input").value;
-        document.getElementById("business_card_email").innerHTML = "Email: " + document.getElementById("form_email_input").value;
+        document.getElementById("business_card_phone").innerHTML = "Tfn: " + document.getElementById("form_phone_input").value;
+        document.getElementById("business_card_email").innerHTML = "Epost: " + document.getElementById("form_email_input").value;
         document.getElementById("business_card").style.backgroundColor = document.getElementById("form_background_color_select").value;
         document.getElementById("business_card").style.color = document.getElementById("form_text_color_select").value;
         document.getElementById("business_card").style.fontFamily = document.getElementById("form_font_select").value;
 
 
-        // Reset form fields
+        // Tidigare kod som återställde inputfälten men kände att reset-knappen var nog
         /*
         document.getElementById("form_company_input").value = "";
         document.getElementById("form_last_name_input").value = "";
@@ -32,8 +32,8 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
-
-    document.getElementById("reset_button").addEventListener("click", function () { // When reset button clicked; hide card and reset form fields 
+    // Gömmer kortet och återsätter inputfälten när nollställ-knappen trycks
+    document.getElementById("reset_button").addEventListener("click", function () {
         document.getElementById("business_card").style.display = "none";
         document.getElementById("form_company_input").value = "";
         document.getElementById("form_first_name_input").value = "";
